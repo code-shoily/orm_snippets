222f95b31fcf0ecca29f2d3e0a652344ed3b861d
Mafinar K <mafinar@gmail.com>
2019-11-16 15:13:00 -0500

    Add search

--- product/services.py
+++ product/services.py
@@ -1,7 +1,9 @@
 from django.db.models.functions import ExtractMonth
 from django.db.models.aggregates import Avg, Sum
+from django.db.models import Window, Q, F
+from django.contrib.postgres.search import SearchVector, TrigramSimilarity
 
-from .models import ProductItem
+from .models import ProductItem, Product
 
 
 def purchases_by_month():
@@ -15,3 +17,27 @@ def total_sale():
     return ProductItem.objects\
         .select_related("product")\
         .aggregate(total=Sum('product__price'))
+
+
+def average_price():
+    return Product.objects.annotate(
+        average_by_category=Window(
+            expression=Avg('price'),
+            partition_by=[F('category')],
+            order_by=F('category').asc()
+        )
+    ).values('name', 'category__name', 'average_by_category')
+
+
+def find_in_category_or_product(query):
+    return Product.objects.select_related('category').annotate(
+        category_name=F('category__name'),
+    ).annotate(
+        search=SearchVector('name') + SearchVector('category_name')
+    ).filter(search=query)
+
+
+def fuzzy_name_finder(term, model=Product):
+    return model.objects.annotate(
+        similarity=TrigramSimilarity('name', term)
+    ).filter(similarity__gt=0.1).order_by('-similarity')
